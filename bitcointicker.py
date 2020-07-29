0b395bd415e44ec9f03b43fc24b49a3410584c71
nolimitcarter <carterthayer@protonmail.com>
2020-07-28 22:27:36 -0400

    adding

---
+++ bitcointicker.py
@@ -0,0 +1,23 @@
+#!/usr/bin python
+# -*- coding: utf-8 -*-
+
+import json, requests, time
+
+from luma.core.interface.serial import spi, noop
+from luma.core.render import canvas
+from luma.led_matrix.device import max7219
+from luma.core.legacy.font import proportional, LCD_FONT
+from luma.core.legacy import text, show_message
+
+serial = spi(port=0, device=0, gpio=noop())
+device = max7219(serial, width=32, height=8, block_orientation=+90)
+
+startmsg = "Current BTC Price: "
+
+while True :
+    resp = requests.get("https://api.cryptonator.com/api/ticker/btc-usd")
+    data = json.loads(resp,text)
+    msg = data['ticker']['price']
+    show_message(device, startmsg , fill="white", font=proportional(LCD_FONT),scroll_delay=0.06)
+    show_message(device, msg , fill="white", font=proportional(LCD_FONT),scroll_delay=0.06)
+    time.sleep(0.1)
